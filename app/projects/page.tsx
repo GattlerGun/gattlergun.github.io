77a17da7e14e1f1ebd92c7aaf5c764db8127ad59
Alexey Rybkin <111182781+GattlerGun@users.noreply.github.com>
2024-03-19 11:35:12 +1000

    update Projects Page & add transition in footer nav

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,13 +1,40 @@
 import { Metadata } from "next";
-import { Projects } from "../ui/projects/projects";
+import styles from '@/app/ui/projects/projects.module.scss';
+import clsx from "clsx";
+import Link from "next/link";
+import { Project } from "../ui/projects/project";
+import { fetchProjects } from "@/lib/data";
+
 
 export const metadata: Metadata = {
     title: `Projects | Alexey Rybkin`,
-    description: "It,s my project",
+    description: "It,s projects by Aleksey Rybkin",
 };
 
-export default function Page() {
+export default async function Page() {
+    const projectsList = await fetchProjects();
     return (
-        <Projects />
+        <section className={styles.projects} id='projects'>
+        <div className='container'>
+            <div className={styles.projects__inner}>
+                <h2 className={clsx(styles.projects__title, 'title')}>
+                    My Projects
+                </h2>
+                <div className={styles.projects__items}>
+                    {projectsList?.map((project) => {
+                        return (
+                            <Link className={styles.projects__itemLink} key={project.id} href={`/projects/project/${project.id}`}>
+                                <Project
+                                    title={project.title}
+                                    description={project.description}
+                                    img={project.img}
+                                />
+                            </Link>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    </section>
     )
 }
